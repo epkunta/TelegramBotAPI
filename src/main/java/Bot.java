3c5d3e4d44ab958981bd9532dd937fb1e9c79714
epkunta <kee38@protonmail.com>
2018-11-07 18:36:00 +0300

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,92 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        Model model = new Model();
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            System.out.println(message.getChat().getFirstName());
+            System.out.println(message.getText());
+            switch (message.getText()) {
+                case "/start":
+                    sendMsg(message, "Hello!)");
+                    break;
+                case "/help":
+                    sendMsg(message, "How can I help you?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "What should we adjust?");
+                    break;
+                default:
+                    try {
+                        sendMsg(message, Weather.getWeather(message.getText(), model));
+                        System.out.println(Weather.getWeather(message.getText(), model));
+                    } catch (IOException e) {
+                        sendMsg(message, "Such a city isn't found");
+                    }
+            }
+        }
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            setButtons(sendMessage);
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true); // choose whom to show the keyboard
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false); // hidden the keyboard or not
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/settings"));
+
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    }
+
+    public String getBotUsername() {
+        return "WeatherBot";
+    }
+
+    public String getBotToken() {
+        return "Bot Token Here";
+    }
+}
---
+++ src/main/java/Model.java
@@ -0,0 +1,47 @@
+public class Model {
+    private String main;
+    private String name;
+    private String icon;
+    private Double temp;
+    private Double humidity;
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public Double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Double temp) {
+        this.temp = temp;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+}
